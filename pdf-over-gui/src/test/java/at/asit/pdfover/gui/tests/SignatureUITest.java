159185532a0cb45370e4e93ac09b2d1c9a0356af
kathrin.resek <kathrin.resek@a-sit.at>
2025-10-28 12:57:08 +0100

    enable negative tests

--- pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/SignatureUITest.java
+++ pdf-over-gui/src/test/java/at/asit/pdfover/gui/tests/SignatureUITest.java
@@ -14,14 +14,12 @@ public class SignatureUITest extends AbstractSignatureUITest {
         setCredentials();
         testSignature(false, false);
     }
-
-    /*
+    
     @ParameterizedTest
     @EnumSource(Profile.class)
     public void testSignatureAutoPositionNegative() throws IOException {
         setCredentials();
         testSignature(true, false);
     }
-    
-     */
+
 }
